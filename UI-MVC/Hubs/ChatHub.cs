9b59cbe1796479c2a2875d3f60fe7b4862c14fa5
Maxim De Coninck <maxim.deconinck@student.kdg.be>
2018-03-08 11:25:34 +0100

    SIgnalr

--- UI-MVC/Hubs/ChatHub.cs
+++ UI-MVC/Hubs/ChatHub.cs
@@ -8,9 +8,9 @@ namespace SC.UI.Web.MVC.Hubs
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public void Send(string name, string message, string passwd)
         {
-            Clients.All.addNewMessageToPage(name, message);
+            Clients.All.addNewMessageToPage(name, message, passwd);
         }
     }
 }
